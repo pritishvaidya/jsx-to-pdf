a9040a5fe1cf06270fd4794535cc9de811397e6e
Pritish Vaidya <pritishvaidya94@gmail.com>
2020-12-29 16:46:24 +0530

    Update file changes

--- example/src/App.js
+++ example/src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
+
 import './App.css';
+
+import { JsxToPdf } from "jsx-to-pdf";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <JsxToPdf>
+      {({ save, jsxRef, error, errorText }) => {
+        return (
+          <div className="App" ref={jsxRef}>
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo"/>
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+              <button style={{ height: 50, margin: 20, backgroundColor: '#61dafb' }} onClick={save}>Download PDF for this Page</button>
+            </header>
+          </div>
+        )
+      }}
+    </JsxToPdf>
   );
 }
 
--- src/jsx-to-pdf.js
+++ src/jsx-to-pdf.js
@@ -2,12 +2,16 @@ import { useRef, useState, useEffect } from 'react';
 import JsPDF from 'jspdf';
 
 function JsxToPdf(props) {
-  const { children } = props;
+  const {
+    name, scale, x, y, options, children,
+  } = props;
+
+  const [loading, updateLoading] = useState(false);
   const [error, updateError] = useState(false);
   const [errorText, updateErrorText] = useState('');
 
   const jsxRef = useRef();
-  const doc = new JsPDF();
+  const doc = new JsPDF(options);
 
   useEffect(() => {
     updateError(false);
@@ -20,16 +24,42 @@ function JsxToPdf(props) {
       updateErrorText('JSX Ref must be specified');
     }
 
-    doc.html(jsxRef.current, {
-      callback(pdfDoc) {
-        pdfDoc.save();
-      },
-    });
+    try {
+      updateLoading(true);
+      doc.html(jsxRef.current, {
+        html2canvas: {
+          allowTaint: true,
+          useCORS: true,
+          scale,
+        },
+        callback: async (pdfDoc) => {
+          pdfDoc.save(name);
+        },
+        x,
+        y,
+      });
+    } catch (ex) {
+      updateError(true);
+      updateErrorText(ex);
+      updateLoading(false);
+    }
   }
 
   return children({
-    save: convertToPdf, jsxRef, error, errorText,
+    save: convertToPdf, jsxRef, loading, error, errorText,
   });
 }
+
+JsxToPdf.defaultProps = {
+  name: 'jsxToPdf',
+  scale: 1,
+  x: 0,
+  y: 0,
+  options: {
+    orientation: 'p',
+    unit: 'pt',
+    format: 'a4',
+  },
+};
 
 export default JsxToPdf;
